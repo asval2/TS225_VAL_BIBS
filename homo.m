6f59ccdd7de5f2c778a53cb36b4275f8cad31d88
Vivien RAMAHANDRY <vramahandry@enseirb-matmeca.fr>
2019-11-19 16:40:47 +0100

    homographie

--- homo.m
+++ homo.m
@@ -2,28 +2,28 @@ function [X] = homo(L1,L2)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-%Céationde la matrice B%
+%Creation de la matrice B%
 [n m] = size(L2);
 B=[];
 for i = 1:n
-    B= [B ; L2(n,1)];
-    B= [B ; L2(n,2)];
+    B= [B ; L2(i,1); L2(i,2)];
+    
 end
+B
 
-%Création de la matrice A%
+%Creation de la matrice A%
 A=[];
 for i=1:n
-    x1 = L1(n,1);
-    y1 = L1(n,2);
-    x2 = L2(n,1);
-    y2 = L2(n,2);
+    x1 = L1(i,1);
+    y1 = L1(i,2);
+    x2 = L2(i,1);
+    y2 = L2(i,2);
     A= [A; x1 x2 1 0 0 0 -x1*x2 -y1*x2];
     A= [A; 0 0 0 x1 y1 1 -x1*y2 -y1*y2 ];
 end
 X = A\B;
 
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
 end
 
---
+++ main.m
@@ -0,0 +1,29 @@
+clear all;
+close all;
+clc;
+A = imread('./images/lena.bmp');
+[h, w] = size(A);
+figure,imshow(A);
+L1 = ginput(4);
+
+B = imread('./images/barbara.bmp');
+[h_, w_] = size(B);
+figure,imshow(B);
+L2 = ginput(4);
+
+H = homo(L1, L2);
+H = [H; 1];
+H = reshape(X,3,3);
+L1_ = [];
+L2_ = [];
+s = 1;
+
+for i = 1:length(L1)
+    L1_ = [L1_; homogene(L1(i,:),s)];
+end
+
+for i = 1:length(L1)
+    L2_ = [L2_; homogene(L1(i,:),s)];
+end
+
+[X Y] = meshgrid(1:h,1:w);
