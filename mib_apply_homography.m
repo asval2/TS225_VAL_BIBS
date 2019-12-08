95a69896e25bac686e54e38d5a8071262096c393
Vivien RAMAHANDRY <vramahandry@enseirb-matmeca.fr>
2019-12-08 17:50:08 +0100

    mib_homography marche pas

--- mib_apply_homography.m
+++ mib_apply_homography.m
@@ -2,41 +2,60 @@ function [MIB2] = mib_apply_homography(MIB1,H)
 %% Homographie à la boite englobante
 M1 = MIB1.M;
 I1 = MIB1.I;
-[ h w ] = size(I1);
+h =  size(I1,1);
+w = size(I1,2);
+
 B = MIB1.B;
-born1=[B(1,1):B(1,1)+h];
-born2=[B(1,2):B(1,2)+w];
-B1=B(1,:);
-B2=B(2,:);
-[ n m ]=B1-B2;
-B3=B1 + [n 0];
-B4= B1 +[0 m];
+% born1=[B(1,1):B(1,1)+h];
+% born2=[B(1,2):B(1,2)+w];
+
+B1=B(1,:)
+B2=B(2,:)
+
+s=B2-B1+1;
+n=s(1);
+m=s(2);
+B3=B1 + [n-1 0];
+B4= B1 +[0 m-1];
 L_1=H*homogene(B1,1);
 L_2=H*homogene(B2,1);
 L_3=H*homogene(B3,1);
 L_4=H*homogene(B4,1);
 
-B_=[ floor(L_1(1:2)/L_1(3)) ; floor(L_2(1:2)/L_2(3)) ; floor(L_3(1:2)/L_3(3)) ; floor(L_4(1:2)/L_4(3)) ];
+B_=[ (L_1(1:2)/L_1(3))';  (L_2(1:2)/L_2(3))';  (L_3(1:2)/L_3(3))';  (L_4(1:2)/L_4(3))' ]
 
 
 
-MIB2.B= [ min(B_) ; max(B_)];
+MIB2.B= [ min(round(B_)) ; max(floor(B_))];
+B_=MIB2.B
 
 % I2  =extraction(I1,H,max(B_)-min(B_))
 %% Projection de 1 sur 2
-[ n m ] = max(B_)-min(B_);
+s2 = B_(2,:)-B_(1,:)+1;
+n = s2(2)
+m = s2(1)
 Mask=zeros(n,m);
-I2=zeros(n,m);
+I2=ones(n,m);
 invH=inv(H);
-for i=B1(1):B1(1)+n
-    for j= B1(2):B1(2)+m
+for i= B_(1,2):B_(2,2)
+    for j=B_(1,1):B_(2,1)
+
+%         i
+%         j
         M_2 = invH*homogene([i;j],1);
         M2= floor(M_2(1:2)/M_2(3));
-        if (ismember(M2(1),born1)) && (ismember(M2(2),born2))
-            I2(i-B1(1),j-B1(2),:)=I1(M2(1),M2(2),:);
-            Mask(i-B1(1),j-B1(2))=1;
+        x = M2(1);
+        y = M2(2);
+        
+        if (x>=B1(2) && x<=B2(2) && y>=B1(1) && y<=B2(1))
+%             i-B_(1,2)+1
+%             j-B_(1,1)+1
+            I2(i-B_(1,2)+1,j-B_(1,1)+1) = I1(x,y);
+            Mask(i-B_(1,2)+1,j-B_(1,1)+1) = 1;
         end
+        
     end
+    
 end
 
 MIB2.M=Mask;
